30871e516b6fa0ff9ec42c21ca9629086ff13148
kmurata798 <kmurata798@gmail.com>
2020-09-20 21:22:56 -0700

    first-commit

---
+++ steganography.py
@@ -0,0 +1,50 @@
+"""
+[Day 7] Assignment: Steganography
+    - Turn in on Gradescope (https://make.sc/bew2.3-gradescope)
+    - Lesson Plan: https://make-school-courses.github.io/BEW-2.3-Web-Security/#/Lessons/Steganography
+
+Deliverables:
+    1. All TODOs in this file.
+    2. Decoded sample image with secret text revealed
+    3. Your own image encoded with hidden secret text!
+"""
+# TODO: Run `pip3 install Pillow` before running the code.
+from PIL import Image
+
+
+def decode_image(path_to_png):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    # Open the image using PIL:
+    encoded_image = Image.open(path_to_png)
+
+    # Separate the red channel from the rest of the image:
+    red_channel = encoded_image.split()[0]
+
+    # Create a new PIL image with the same size as the encoded image:
+    decoded_image = Image.new("RGB", encoded_image.size)
+    pixels = decoded_image.load()
+    x_size, y_size = encoded_image.size
+
+    # TODO: Using the variables declared above, replace `print(red_channel)` with a complete implementation:
+    for x in range(x_size):
+        for y in range(y_size):
+            # => .getpixel returns the pixel value at a given position
+            # CONFUSED on what the & operator is doing...
+            if red_channel.getpixel((x, y)) & 1:
+                pixels[x, y] = (0, 0, 0)
+            else:
+                pixels[x, y] = (255, 255, 255)
+    # print(red_channel)  # Start coding here!
+
+    # DO NOT MODIFY. Save the decoded image to disk:
+    decoded_image.save("decoded_image.png")
+
+
+def encode_image(path_to_png):
+    """
+    TODO: Add docstring and complete implementation.
+    """
+    pass
+decode_image("encoded_sample.png")
